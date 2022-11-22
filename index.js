ad75052c90d742fa3104f2ef392d3bd8c8855019
sjamescarter <stephenjamescarter@gmail.com>
2022-11-21 22:51:41 -0600

    commit tree 5046ba490967a8bf769546275610ea23059eede7

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ft){
+  if (ft <= 400) {
+    return "This one is on me!"
+  } else if (ft <= 2000) {
+    return "That will be twenty bucks."
+  } else if (ft <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(coin){
+  switch (coin) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."; 
+  }
 }
